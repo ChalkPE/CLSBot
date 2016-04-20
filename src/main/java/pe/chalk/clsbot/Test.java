d8f399462c964aed4f4b05434e3638cb2bfd7a23
ChalkPE <chalkpe@gmail.com>
2016-04-21 08:12:55 +0900

    Added test

---
+++ src/main/java/pe/chalk/clsbot/Test.java
@@ -0,0 +1,13 @@
+package pe.chalk.clsbot;
+
+import java.util.Calendar;
+
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2016-03-22
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println(DateCounter.count(2016, Calendar.MARCH, 22));
+    }
+}
